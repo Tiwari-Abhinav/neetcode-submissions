b79a134023ac336b3c356d9ca35e440e77e3a334
Abhinav Tiwari <148998149+Tiwari-Abhinav@users.noreply.github.com>
2026-06-24 12:30:03 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,32 @@
+class Codec:
+    def encode(self, strs: list[str]) -> str:
+        """Encodes a list of strings to a single string."""
+        encoded_str = ""
+        for s in strs:
+            # Format: [length] + [#] + [string]
+            encoded_str += f"{len(s)}#{s}"
+        return encoded_str
+
+    def decode(self, s: str) -> list[str]:
+        """Decodes a single string to a list of strings."""
+        res = []
+        i = 0
+        
+        while i < len(s):
+            # Find the delimiter to know where the length ends
+            j = i
+            while s[j] != '#':
+                j += 1
+            
+            # Extract the length of the upcoming string
+            length = int(s[i:j])
+            
+            # Extract the actual string using the length
+            start_of_str = j + 1
+            end_of_str = start_of_str + length
+            res.append(s[start_of_str:end_of_str])
+            
+            # Move the pointer to the start of the next encoded block
+            i = end_of_str
+            
+        return res
